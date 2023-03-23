a4a1dfce890971958c19b2b9643924232e29a53d
sy5938 <18289665799@163.com>
2023-03-23 18:42:59 +0800

    函数式编程语言dun

--- test2.R
+++ test2.R
@@ -5,3 +5,17 @@
 # 2-3
 
 # 2-4 ,bush
+
+glob2rx("*.shp")
+
+apropos("read")
+
+col_summary <- function(df,dun){
+  out <- vector("double",length(df))
+  for (i in seq_along(df)) {
+    out[i] <- dun(df[[i]])
+  }
+  out
+}
+
+col_summary(dat,mean)
